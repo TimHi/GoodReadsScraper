c81caacc2344aff33810e36de683bee19780a300
TimHi <TimHiller@live.de>
2023-02-14 01:55:06 +0100

    parse book urls

--- src/scraper/listScraper.go
+++ src/scraper/listScraper.go
@@ -2,10 +2,8 @@ package scraper
 
 import (
 	"context"
-	"fmt"
 	"log"
 
-	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 	"github.com/timhi/goodreadscraper/m/v2/src/model"
 )
@@ -15,21 +13,16 @@ func ScrapeList(ctx context.Context, id string) []model.Book {
 	//https://www.goodreads.com/author/list/566
 	//visit the page is missing!
 	//bookIDs := []string{}
-	var nodes []*cdp.Node
+	var bookURLs []string
 	err := chromedp.Run(ctx, chromedp.Tasks{
 		chromedp.Navigate(`https://www.goodreads.com/author/list/` + id),
-		chromedp.Nodes(`tr`, &nodes, chromedp.ByQueryAll),
+		chromedp.WaitVisible(`.tableList`),
+		chromedp.Evaluate(`Array.from(document.querySelectorAll('.tableList tr a.bookTitle')).map(el => el.href)`, &bookURLs),
 	})
+
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to extract hrefs: %v", err)
 	}
 
-	bookURLs := []string{}
-	for _, node := range nodes {
-
-	}
-
-	fmt.Println(len(nodes))
-	fmt.Println(nodes[0].Attributes)
 	return books
 }
